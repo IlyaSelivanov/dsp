4062fedde241cee393d97b6b7a06f1ddcdfc9a20
IlyaSelivanov <selivanov.ilya@gmail.com>
2023-04-10 11:02:00 +0300

    plotter added

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 mod signal;
+mod charts;
 
 use crate::signal::*;
+use crate::charts::*;
 
-fn main() {
-    let signal: Vec<i64> = match Statistics::generate_random_vector(1000, (0, 1)) {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let signal: Vec<i64> = match Statistics::generate_random_vector(1_000_000, (0, 10)) {
         Some(vec) => vec,
         None => Vec::new(),
     };
@@ -12,4 +14,8 @@ fn main() {
     signal.print_info();
 
     // println!("{:?}", signal.data());
+
+    example_chart()?;
+
+    Ok(())
 }
